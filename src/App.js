23bb694fd12d98d2dc4c8d64d9995671afa4bda7
Radhithya <kondaveetiradithyareddy@sau.edu>
2019-08-22 11:47:25 -0500

    CSS Updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="form-style-8">
+      <h2>FEEDBACK FORM</h2>
+    </div>
+  );
+}
+
+export default App;
